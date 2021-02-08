2c2baf63db24654cacce2c56efb6a208972f1da3
Caitlin <caitlinlamprecht@outlook.com>
2021-02-08 13:09:32 -0600

    updated connection link for mongoDB

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const mongoURI = 
     process.env.NODE_ENV === `production`
         ? process.env.DB_URL
-        : `mongodb://localhost:27017/asc`
+        // : `mongodb://localhost:27017/asc`
+        : `mongodb+srv://clamprecht:atlasclammy@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&ssl=true`
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
