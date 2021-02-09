69746b53d1ae9f01160cef1d807d789a80f7db17
Caitlin <caitlinlamprecht@outlook.com>
2021-02-09 16:18:53 -0600

    updated routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -3,22 +3,22 @@ const router = express.Router()
 const User = require('../db/models/User')
 
 //get all users
-router.get('/users', (req,res) => {
+router.get('/', (req,res) => {
     const results = User.find({})
     results.then((users) => {
-        res.render('users/index', {users:users})
+        res.render('/', {users:users})
     })
 })
 
 //get users by id
-router.get('/users/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     User.findById(req.params.id)
     .then((user) => res.json(user))
     .catch(next)
 })
 
 //create a user
-router.post('/users', (req, res, next) => {
+router.post('/', (req, res, next) => {
     User.create(req.body)
         .then((user) => res.json(user))
         .catch(next)
