c80754b83b5d8a1e4f260e9d354d6b1a1fbd550e
Caitlin <caitlinlamprecht@outlook.com>
2021-02-09 16:33:46 -0600

    updated mongo uri

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoURI = 
     process.env.NODE_ENV === `production`
         ? process.env.DB_URL
-        : `mongodb+srv://${process.env.ATLAS_U}:${process.env.ATLAS_P}@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+        : `mongodb+srv://${process.env.ATLAS_U}:${process.env.ATLAS_P}@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&ssl=true`
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
