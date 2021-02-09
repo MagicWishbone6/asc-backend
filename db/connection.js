6f0ab9cad81bcac7c3a390223cae66ebcd15e228
Caitlin <caitlinlamprecht@outlook.com>
2021-02-09 15:15:25 -0600

    created .env with vars

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+
 const mongoURI = 
     process.env.NODE_ENV === `production`
         ? process.env.DB_URL
-        : `mongodb+srv://clamprecht:atlasclammy@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+        : `mongodb+srv://${process.env.ATLAS_U}:${process.env.ATLAS_P}@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
