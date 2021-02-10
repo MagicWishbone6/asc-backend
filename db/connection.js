1629e3b700383adf0c9df6707b1c9b702fe4cd79
Caitlin <caitlinlamprecht@outlook.com>
2021-02-09 18:44:27 -0600

    edited connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const mongoURI = 
     process.env.NODE_ENV === `production`
         ? process.env.DB_URL
         : `mongodb+srv://${process.env.ATLAS_U}:${process.env.ATLAS_P}@cluster0.6kf2m.mongodb.net/asc?authSource=admin&replicaSet=atlas-f955wg-shard-0&readPreference=primary&ssl=true`
+        // console.log(mongoURI)
+        // console.log(process.env.ATLAS_P)
+        console.log(require('dotenv').config())
 
-mongoose.connect(mongoURI || 'mongodb://localhost:27017/asc', {
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
